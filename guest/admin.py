b7c6f487bb0327d656a469086abcde0b5a08e707
yuukisfurue <larayuuki4908908@gmail.com>
2023-05-06 16:12:09 +0900

    Initial Commit

---
+++ guest/admin.py
@@ -0,0 +1,34 @@
+from django.contrib import admin
+from import_export import resources
+from import_export.admin import ImportExportActionModelAdmin
+from import_export.formats import base_formats
+from guest.models import Guest
+
+
+# Register your models here.
+class GuestResource(resources.ModelResource):
+    class Meta:
+        model = Guest
+    fields = ['id','name','prefecture','gender','employmentstatus','company','jyob','stay','affiliation','postion','annual','lastyear']
+    import_id_fields = ['id']
+
+class GuestAdmin(ImportExportActionModelAdmin):
+    list_display = (
+        'id',
+        'name',
+        'prefecture',
+        'gender',
+        'employmentstatus',
+        'company',
+        'jyob',
+        'stay',
+        'affiliation',                        
+        'postion',
+        'annual',
+        'lastyear'
+      )
+
+    resource_class = GuestResource
+    formats = [base_formats.CSV]
+
+admin.site.register(Guest, GuestAdmin)
